49be30d4afff55f2a1981a70de081e6b4667526f
Dariya <dkladova@list.ru>
2022-10-24 19:59:41 +0300

    crate progect

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string[] arrayOne = new string[4] { "1234", "1567", "-5", "Hollo" };
+string[] arraySecond = new string[arrayOne.Length];
+
+void SecondArrayEND(string[] arrayOne, string[] arraySecond)
+{
+    int count = 0;
+    for (int i = 0; i < arrayOne.Length; i++)
+    {
+        if (arrayOne[i].Length <= 3)
+        {
+            arraySecond[count] = arrayOne[i];
+            count++;
+        }
+    }
+
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayEND(arrayOne,arraySecond);
+ PrintArray(arraySecond);
+
